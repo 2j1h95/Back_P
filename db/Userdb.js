ff0e8537eb3397b69b7ce08777629254a6121efb
정종헌 <mainvoid95@gmail.com>
2020-02-03 21:36:14 +0900

    회원가입 기능 테스트

--- db/Userdb.js
+++ db/Userdb.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({  
     name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true},
+    pass: {type: String, required: true},
 });
 
 mongoose.model('User', UserSchema);
