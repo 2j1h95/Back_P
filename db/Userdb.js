6af5d78ccefbefac62cadc171c7744c6d19f5422
정종헌 <mainvoid95@gmail.com>
2020-02-03 21:23:38 +0900

    회원가입 기능 테스트

--- db/Userdb.js
+++ db/Userdb.js
@@ -5,7 +5,6 @@ var UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
-    image : imageSchema
 });
 
 mongoose.model('User', UserSchema);
